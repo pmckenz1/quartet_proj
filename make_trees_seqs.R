b090d00f461a44ac7f942832856e01b817e1746c
pmckenz1 <pmckenz1@utk.edu>
2017-10-17 21:58:57 -0400

    Added poster-making notebook and materials for mobot2017 conference, also added qmc_test notebook

--- make_trees_seqs.R
+++ make_trees_seqs.R
@@ -1,13 +1,19 @@
-for (i in 1:2000) {
+library(ape)
+for (i in 1:3000) {
   write.tree(rcoal(10),paste0("/Users/pmckenz1/Desktop/projects/quartet_proj/random_trees/samp",i,".phy"))
 }
 
 for (i in 1:2000) {
-  system(paste0("seq-gen -mGTR -l10000 -n1 -z12345 < Desktop/projects/quartet_proj/random_trees/samp",i,".phy > Desktop/projects/quartet_proj/tree_seqs/test",i,".dat"))
+  system(paste0("seq-gen -mGTR -l420 -n1 -z12345 < /Users/pmckenz1/Desktop/projects/quartet_proj/random_trees/samp",i,".phy > /Users/pmckenz1/Desktop/projects/quartet_proj/tree_seqs/test",i,".dat"))
+}
+for (i in 1:1000) {
+  system(paste0("seq-gen -mGTR -l10000 -n1 -z12345 < /Users/pmckenz1/Desktop/projects/quartet_proj/random_trees/samp",(i+2000),".phy > /Users/pmckenz1/Desktop/projects/quartet_proj/tree_seqs/test",(i+2000),".dat"))
 }
 
 getwd()
 
+plot(rtree(37))
+write.tree(rtree(37),paste0("/Users/pmckenz1/Desktop/projects/quartet_proj/random_trees/tree37.phy"))
 
-
-
+mytree <- read.tree("/Users/pmckenz1/Desktop/projects/quartet_proj/random_trees/tree37.phy")
+cophenetic(mytree)["t2", "t4"]
