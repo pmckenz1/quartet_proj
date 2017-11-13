288db8ff63b4f21dc3effdb03359507a23731504
pmckenz1 <pmckenz1@utk.edu>
2017-11-13 16:53:06 -0500

    Finished machine learning proof-of-concept notebook

--- make_trees_seqs.R
+++ make_trees_seqs.R
@@ -4,7 +4,7 @@ for (i in 1:3000) {
 }
 
 for (i in 1:2000) {
-  system(paste0("seq-gen -mGTR -l420 -n1 -z12345 < /Users/pmckenz1/Desktop/projects/quartet_proj/random_trees/samp",i,".phy > /Users/pmckenz1/Desktop/projects/quartet_proj/tree_seqs/test",i,".dat"))
+  system(paste0("seq-gen -mGTR -l484 -s .1 -n1 -z12345 < /Users/pmckenz1/Desktop/projects/quartet_proj/random_trees/samp",i,".tre > /Users/pmckenz1/Desktop/projects/quartet_proj/tree_seqs/test",i,".dat"))
 }
 for (i in 1:1000) {
   system(paste0("seq-gen -mGTR -l10000 -n1 -z12345 < /Users/pmckenz1/Desktop/projects/quartet_proj/random_trees/samp",(i+2000),".phy > /Users/pmckenz1/Desktop/projects/quartet_proj/tree_seqs/test",(i+2000),".dat"))
